2893ca498fb54fc0dde51c51f3c6fe0cdeec88a8
JaleaReal-1 <jalpawel753@gmail.com>
2026-05-15 08:19:26 -0500

    commit de arreglo rqts

--- app/core/config.py
+++ app/core/config.py
@@ -8,13 +8,30 @@ load_dotenv()
 DATABASE_URL = (
     os.getenv("DATABASE_URL")
     or os.getenv("DATABASE_PUBLIC_URL")
+    or "sqlite:///./test.db"
 )
 
-if not DATABASE_URL:
-    raise RuntimeError("❌ NO HAY DATABASE_URL NI DATABASE_PUBLIC_URL")
-
 SECRET_KEY = os.getenv("SECRET_KEY", "dev")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
-BASE_UPLOAD_DIR = os.path.join(os.getcwd(), "uploads", "ordenes_compra")
-os.makedirs(BASE_UPLOAD_DIR, exist_ok=True)
+
+ALGORITHM = os.getenv(
+    "ALGORITHM",
+    "HS256"
+)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv(
+        "ACCESS_TOKEN_EXPIRE_MINUTES",
+        60
+    )
+)
+
+BASE_UPLOAD_DIR = os.path.join(
+    os.getcwd(),
+    "uploads",
+    "ordenes_compra"
+)
+
+os.makedirs(
+    BASE_UPLOAD_DIR,
+    exist_ok=True
+)
